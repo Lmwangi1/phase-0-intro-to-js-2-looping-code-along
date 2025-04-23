fc6bd4feb85d79837ed53c3db8aa813eb5962d04
lucy <lmwangi619@gmail.com>
2025-04-23 10:03:47 +0300

    New Update

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function countDown(startingNumber) {
+    while (startingNumber >= 0) {
+      console.log(startingNumber);
+      startingNumber--;
+    }
+  }
+  function writeCards(names, event) {
+    const messages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Welcome, ${names[i]}, to this  ${event} home!`);
+    }
+  
+    return messages;
+  }
+  
